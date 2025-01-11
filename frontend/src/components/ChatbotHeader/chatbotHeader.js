946e8ef7dc127060710c3dc4104e8ddedad064c9
sydney_sim_yf <sydney9191@gmail.com>
2025-01-11 15:30:56 +0800

    First Component fixed, dynamic headers

--- frontend/src/components/ChatbotHeader/chatbotHeader.js
+++ frontend/src/components/ChatbotHeader/chatbotHeader.js
@@ -34,7 +34,9 @@ const ChatbotHeader=({
     <header className="chatbot-header">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <h1>ShatGPT</h1>
-    <div>
+
+    <div className="top-right-button-cluster">
+      {/* Start of the profile logic*/}
       <div className="profile-section">
         <img
           src="/user.png"
@@ -59,24 +61,26 @@ const ChatbotHeader=({
           />
         )}
       </div>
+    {/* Start of the tokens used logic*/}
+      <div className="tokens-used">
+        <p>
+          <strong>Tokens Used:</strong> {tokensUsed || "No tokens used yet."}
+        </p>
+        <button className="clear-context" onClick={showContextWarning}>
+          Clear Context
+        </button>
+      </div>
+      {contextWarningVisible && (
+        <ConfirmationWarning
+          yesButtonPress={handleClearContext}
+          noButtonPress={hideContextWarning}
+          warningText={contextClearWarningText}
+          yesButtonText={"Clear Context"}
+          noButtonText={"Cancel"}
+        />
+      )}
     </div>
-    <div className="tokens-used">
-      <p>
-        <strong>Tokens Used:</strong> {tokensUsed || "No tokens used yet."}
-      </p>
-      <button className="clear-context" onClick={showContextWarning}>
-        Clear Context
-      </button>
-    </div>
-    {contextWarningVisible && (
-      <ConfirmationWarning
-        yesButtonPress={handleClearContext}
-        noButtonPress={hideContextWarning}
-        warningText={contextClearWarningText}
-        yesButtonText={"Clear Context"}
-        noButtonText={"Cancel"}
-      />
-    )}
+    
     <div className="conversations-menu">
       <i className="fa-solid fa-bars" onClick={toggleConversationBar}/>
     </div>
